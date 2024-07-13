e3252b1be3e56a3e239446a106d5615f5768b802
Eduardo Mota <83442828+eduamota@users.noreply.github.com>
2024-07-12 23:51:58 -0700

    Added code for lab 3 and 4

--- Lab 4/converse_api.py
+++ Lab 4/converse_api.py
@@ -5,27 +5,35 @@ bedrock = session.client(service_name='bedrock-runtime')
 
 message_list = []
 
-initial_message = {
-    "role": "user",
-    "content": [
-        { "text": "How are you today?" } 
-    ],
-}
+while True:
+    inp = input("User: ")
 
-message_list.append(initial_message)
+    if inp == "exit":
+        break
 
-response = bedrock.converse(
-    modelId="anthropic.claude-3-sonnet-20240229-v1:0",
-    messages=message_list,
-    inferenceConfig={
-        "maxTokens": 2000,
-        "temperature": 0
-    },
-)
+    initial_message = {
+        "role": "user",
+        "content": [
+            { "text": inp } 
+        ],
+    }
 
-response_message = response['output']['message']
-# print(json.dumps(response_message, indent=4))
+    message_list.append(initial_message)
 
-message_list.append(response_message)
+    response = bedrock.converse(
+        modelId="anthropic.claude-3-sonnet-20240229-v1:0",
+        messages=message_list,
+        inferenceConfig={
+            "maxTokens": 2000,
+            "temperature": 0
+        },
+    )
 
-print(json.dumps(message_list, indent=4))
+    response_message = response['output']['message']['content'][0]['text']
+    # print(json.dumps(response_message, indent=4))
+
+    message_list.append(response_message)
+    print(response_message)
+
+    # print(json.dumps(message_list, indent=4))
+
